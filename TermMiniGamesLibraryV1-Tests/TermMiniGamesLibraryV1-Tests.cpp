d435c4d98855ce6d389983f6b8b80de3990921ec
Pankit Thakkar <pankitthakkar007@gmail.com>
2021-03-17 15:59:00 -0400

    Setup Test Project

--- TermMiniGamesLibraryV1-Tests/TermMiniGamesLibraryV1-Tests.cpp
+++ TermMiniGamesLibraryV1-Tests/TermMiniGamesLibraryV1-Tests.cpp
@@ -1,16 +1,78 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 extern "C" {	//Put header files here with your name commented beside them for ownership!
-	//#include "Main.h" -- Pankit	//leave commented until test is linked properly and headers can actually be added
+	#include "Main.h" // Pankit	//leave commented until test is linked properly and headers can actually be added
+	#include "Minesweeper.h" // Anthony
+	#include "RPS.h" // Zahra
+	#include "HangMan.h" // Mo
 }
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TermMiniGamesLibraryV1Tests
 {
-	TEST_CLASS(TermMiniGamesLibraryV1Tests)
+	TEST_CLASS(Main)
 	{
 	public:
 		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(RPS)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(Minesweeper)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(Hangman)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
 		TEST_METHOD(TestMethod1)
 		{
 		}
